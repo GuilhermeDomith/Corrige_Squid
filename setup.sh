033d1d5d84761d1d4ca09c6593382bbd2e54e7fa
guilherme <guilhermedomith@gmail.com>
2019-11-20 15:11:25 -0200

    Passar ip do host a ser corrigido por parametro

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
 
+[ -z $1 ] && ./log.sh -i "HOST NÂO INFORMADO" && ./log.sh -i "EXEMPLO: ./setup.sh 192.168.0.37" && exit 1
 
-sshpass -p admin scp -r ../Corrige_Squid root@192.168.0.37:~/
-sshpass -p admin ssh root@192.168.0.37 -t 'cd ~/Corrige_Squid; /bin/bash corrige_squid.sh; /bin/bash' 
+HOST=$1
+PATH_DIR=$(pwd)
+PATH_DIR_REMOTO=$(echo "~/$(pwd |awk -F"/" '{print $NF}')")
+
+sshpass -p admin scp -r $PATH_DIR root@$HOST:~/
+sshpass -p admin ssh root@$HOST -t "cd $PATH_DIR_REMOTO; /bin/bash corrige_squid.sh; /bin/bash"
+
