f5eabc47fbd29e4a2db4106c330ced81b9490b26
guilhermedomith <guilhermedomith@gmail.com>
2019-11-28 12:53:11 -0300

    [UP] Soma de pontos na correção do Parametros

--- corrige_params.sh
+++ corrige_params.sh
@@ -7,12 +7,15 @@ SQUID_CONF=$1
 
 #./log.sh -table-title "PARÂMETRO"  "ERRO"
 
+PONTOS=0
+
 while read CONF; do
 
-	PARAM=$(echo $CONF | awk '{print $1}')
-	# Obtém o valor mesmo que seja mais de um
-	VALOR=$(echo $CONF | awk '{first = $1; $1 = ""; print $0}')
-
+	PONTO=$(echo $CONF | awk -F")" '{print $1}' | sed -e "s/[^0-9\.]//g") 
+	PARAM=$(echo $CONF | awk -F")" '{first = $1; $1 = ""; print $0}' | awk '{print $2}')
+	# Obtém o valor do parametro, mesmo que seja mais de um
+	VALOR=$(echo $CONF | awk -F")" '{first = $1; $1 = ""; print $0}' | awk '{first = $1; $1 = ""; print $0}')
+	
 	CONF_ALUNO=$(cat $SQUID_CONF | grep $PARAM)
 	ENCONTRADO=$?
 	VALOR_ALUNO=""
@@ -21,14 +24,20 @@ while read CONF; do
 		VALOR_ALUNO=$(echo $CONF_ALUNO | awk '{first = $1; $1 = ""; print $0}')
 	fi
 
+
 	if [ $ENCONTRADO -eq 0 ] && [ "$VALOR" = "$VALOR_ALUNO" ]; then
 		./log.sh -certo "$(printf '%-48s %s\n' "$CONF" "" )" 
 	else
 		DEBUG=$( ([ $ENCONTRADO -eq 0 ] && echo "[$VALOR_ALUNO ]") || echo "[ NÃO ENCONTRADO ]" )
 		./log.sh -errado "$(printf '%-48s %s\n' "$CONF"  "$DEBUG")"
+		PONTO=0
 	fi
 
+	PONTOS=$(($PONTOS + $PONTO))
+
 done < /tmp/corrige_params
+
+./log.sh -title "$(echo "SOMA:" $PONTOS "Pts")"
 
 exit 0
 
--- corrige_usuarios.sh
+++ corrige_usuarios.sh
@@ -11,23 +11,37 @@ grep "http_access" $SQUID_CONF > /tmp/squid_http_access
 
 #./log.sh -table-title "Usuário" "CRIADA ACL" "USADO EM HTTP_ACCESS"
 
-while read USUARIO; do
-	ACL_USUARIO=$(cat /tmp/squid_acls | sed -n "$(echo -e "/acl .* proxy_auth "$USUARIO"/p")")
+PONTOS=0
+while read CONF_USUARIO; do
+
+	PONTO=$(echo $CONF_USUARIO | awk -F")" '{print $1}' | sed -e "s/[^0-9\.]//g")
+        USUARIO=$(echo $CONF_USUARIO | awk -F")" '{print $2}')
+	
+	echo $PONTO '|'
+	echo $USUARIO '|'
+
+	ACL_USUARIO=$(cat /tmp/squid_acls | sed -n "$(echo -e "/acl .* proxy_auth $USUARIO/p")")
+	echo $ACL_USUARIO '|'
 	ACL_USUARIO=$(echo $ACL_USUARIO | awk '{print $2}')
 
 	if [ -z $ACL_USUARIO ]; then
-		./log.sh -errado "$(printf '%-48s %-20s %-10s\n' $USUARIO "" "")"
+		./log.sh -errado "$(printf '%-48s %-20s %-10s\n' "$CONF_USUARIO" "" "")"
+		PONTO=0
 	else
 		grep $ACL_USUARIO /tmp/squid_http_access &> /dev/null
 		HTTP_ACCESS_USUARIO=$?
 
 
 		if [ $HTTP_ACCESS_USUARIO -eq 0 ]; then
-			./log.sh -certo "$(printf '%-48s %-20s %-10s\n' $USUARIO "[ $ACL_USUARIO ]" "[ CRIADO ]")"
+			./log.sh -certo "$(printf '%-48s %-20s %-10s\n' "$CONF_USUARIO" "[ $ACL_USUARIO ]" "[ CRIADO ]")"
 		else
-			./log.sh -errado "$(printf '%-48s %-20s %-10s\n' $USUARIO "[ $ACL_USUARIO ]" "")"
+			./log.sh -errado "$(printf '%-48s %-20s %-10s\n' "$CONF_USUARIO" "[ $ACL_USUARIO ]" "")"
+			PONTO=0
 		fi
 	fi
 	
+	PONTOS=$(($PONTOS + $PONTO))
 done < /tmp/corrige_usuarios
+
+./log.sh -title "$(echo "SOMA:" $PONTOS "Pts")"
 
