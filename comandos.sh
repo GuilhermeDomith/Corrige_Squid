ca381b4bcc9df0c653eee1c4b3f40f0c20b2aadd
guilherme <guilhermedomith@gmail.com>
2019-12-05 02:28:20 -0200

    [UP] Arquivo Removido

--- comandos.sh
+++
@@ -1,7 +0,0 @@
-sed -e /^s*#.*$/d -e /^s*$/d /etc/squid/squid.conf
-#./comandos.sh /etc/squid/squid.conf.default | sed  /acl Safe_ports port 21s*#.*$/d
-#./comandos.sh /etc/squid/squid.conf.default | sed  /acl Safe_ports port 21.*/d
-cat /etc/squid/squid.conf | egrep "^maximum_object_size" | sed s/[a-zA-Z_\s]//g
-cat /etc/squid/squid.conf | egrep "^maximum_object_size" | cut -f2-3 -d 
-N=$(cat /etc/squid/squid.conf | egrep "^maximum_object_size" | awk {print })
-[ $N == "258MB" ]
